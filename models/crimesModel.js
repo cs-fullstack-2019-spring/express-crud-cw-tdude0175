4ed7ec8ec4fb8312473e718902e31b1fd615c490
Kenn-CodeCrew <40698252+Kenn-CodeCrew@users.noreply.github.com>
2019-03-24 22:28:13 -0500

    Update crimesModel.js

--- models/crimesModel.js
+++ models/crimesModel.js
@@ -6,29 +6,6 @@ var Schema = mongoose.Schema;
 var CrimeSchema = new Schema(
     {
         //Enter model information here
-        category: [{
-            code: String,
-            name: String,
-        }],
-        date: Date,
-        person_id: String,
-        crime: [{
-            category: String,
-            location_type: String,
-            location: [{
-                latitude: Number,
-                street: [{
-                    id: Number,
-                    street: String,
-                }],
-                longitude: Number,
-            }]
-        }],
-        context: String,
-        persistent_id: String,
-        id: Number,
-        location_subtype: String,
-        month: String,
     });
 
 //Export model
